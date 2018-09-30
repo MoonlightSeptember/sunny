ef347930387f7cecb5bffdc62a9b891ed058a4e6
MoonlightSeptember <43489831+MoonlightSeptember@users.noreply.github.com>
2018-09-30 17:38:38 +0800

    Add files via upload
    计算面积函数

---
+++ area.py
@@ -0,0 +1,14 @@
+##计算面积函数
+def area(width,height):
+...   return width*height
+...
+>>> def print_welcome(name):
+...     print("Welcome",name)
+...
+>>> print_welcome("jane")
+Welcome jane
+>>> w=4
+>>> h=5
+>>> print("width=",w,"height=",h,"area=",area(w,h))
+width= 4 height= 5 area= 20
+>>>
